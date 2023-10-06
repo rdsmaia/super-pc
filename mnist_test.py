2ba2c151544830082325b499a703ce3274feb94e
RANNIERY DA SILVA MAIA <rmaia@linse.ufsc.br>
2023-10-06 11:22:56 -0300

    adding scripts

---
+++ mnist_test.py
@@ -0,0 +1,115 @@
+import torch
+import torch.nn as nn
+from torchvision import datasets
+from torchvision.transforms import ToTensor
+from torch import optim
+from torch.autograd import Variable
+
+
+def train(num_epochs, cnn, loaders):
+    
+    cnn.train()
+        
+    # Train the model
+    total_step = len(loaders['train'])
+        
+    for epoch in range(num_epochs):
+        for i, (images, labels) in enumerate(loaders['train']):
+            
+            # gives batch data, normalize x when iterate train_loader
+            b_x = Variable(images)   # batch x
+            b_y = Variable(labels)   # batch y
+            output = cnn(b_x)[0]               
+            loss = loss_func(output, b_y)
+            
+            # clear gradients for this training step   
+            optimizer.zero_grad()           
+            
+            # backpropagation, compute gradients 
+            loss.backward()    
+            # apply gradients             
+            optimizer.step()                
+            
+            if (i+1) % 100 == 0:
+                print ('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}' 
+                       .format(epoch + 1, num_epochs, i + 1, total_step, loss.item()))
+            pass
+        pass
+    pass
+
+class CNN(nn.Module):
+    def __init__(self):
+        super(CNN, self).__init__()
+        self.conv1 = nn.Sequential(         
+            nn.Conv2d(
+                in_channels=1,              
+                out_channels=16,            
+                kernel_size=5,              
+                stride=1,                   
+                padding=2,                  
+            ),                              
+            nn.ReLU(),                      
+            nn.MaxPool2d(kernel_size=2),    
+        )
+        self.conv2 = nn.Sequential(         
+            nn.Conv2d(16, 32, 5, 1, 2),     
+            nn.ReLU(),                      
+            nn.MaxPool2d(2),                
+        )
+        # fully connected layer, output 10 classes
+        self.out = nn.Linear(32 * 7 * 7, 10)
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.conv2(x)
+        # flatten the output of conv2 to (batch_size, 32 * 7 * 7)
+        x = x.view(x.size(0), -1)       
+        output = self.out(x)
+        return output, x    # return x for visualization
+
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+print(f'Current device being used: {device}\n')
+
+train_data = datasets.MNIST(
+    root = 'data',
+    train = True,                         
+    transform = ToTensor(), 
+    download = True,            
+)
+test_data = datasets.MNIST(
+    root = 'data', 
+    train = False, 
+    transform = ToTensor()
+)
+
+print(f'Training data: {train_data}\n')
+print(train_data.data.size())
+
+print(f'Test data: {test_data}\n')
+print(train_data.targets.size())
+
+from torch.utils.data import DataLoader
+loaders = {
+    'train' : torch.utils.data.DataLoader(train_data, 
+                                          batch_size=100, 
+                                          shuffle=True, 
+                                          num_workers=1),
+    
+    'test'  : torch.utils.data.DataLoader(test_data, 
+                                          batch_size=100, 
+                                          shuffle=True, 
+                                          num_workers=1),
+}
+print(f'Loaders: {loaders}\n')
+
+cnn = CNN()
+print(f'Model: {cnn}\n')
+
+loss_func = nn.CrossEntropyLoss()
+print(f'Loss function: {loss_func}\n')
+
+optimizer = optim.Adam(cnn.parameters(), lr = 0.01)   
+print(f'Optimizer: {optimizer}\n')
+
+num_epochs = 10
+train(num_epochs, cnn, loaders)
---
+++ run_mnist_test.sh
@@ -0,0 +1,10 @@
+#!/bin/bash 
+#SBATCH --job-name=gpu_check
+#SBATCH --time=0-0:10
+#SBATCH --partition=gpu
+#SBATCH --exclusive
+
+# informando ao tch-rs que desejo compilar com cuda na versão 11.7
+export TORCH_CUDA_VERSION=cu117
+
+python mnist_test.py
